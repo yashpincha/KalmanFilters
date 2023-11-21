707985fe578ed3f2ba8f59ae8212c0bee89116b0
Yash Pincha <142161252+yashpincha@users.noreply.github.com>
2023-11-21 13:25:31 +0530

    Update randomWalk.m

--- src/randomWalk.m
+++ src/randomWalk.m
@@ -2,6 +2,8 @@
 A heuristic implementation of a random walk process
 Based on Bolviken, E., Christophersen, N., & Storvik, G. (1998). Linear dynamical models, Kalman filtering and
 statistics: Lecture notes to IN-ST 259. University of Oslo. https://www.mn.uio.no/math/tjenester/kunnskap/kompendier/komp_kalman.pdf
+
+Q(k − 1) = 1 for k = 1, ..., 50 and = 0.1 for k = 51, ..., 100. Observations are simulated with R(k)=0.5 for all k.
 %}
 
 randn('state'); % Initialize the random number generator state
